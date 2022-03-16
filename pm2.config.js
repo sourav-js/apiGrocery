8f19e1bed895af226443f118b6cb4a235338de8a
sourav-js <oop.gupta12345@gmail.com>
2022-03-16 23:18:17 +0530

    commiting

---
+++ pm2.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apps : [{
+      name   : "apigroceryji",
+      script : "nodemon grocery.js",
+      error_file : "/home/sourav/sourav/apigroceryJi_error.log",
+      out_file : "/home/sourav/sourav/apigroceryJi_out.log"
+    }]
+  }
